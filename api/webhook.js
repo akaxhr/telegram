6c216450a750bce8475617625df377ac455e6adc
Akash R <imakash566@gmail.com>
2026-06-12 20:27:59 +0530

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -23,6 +23,7 @@ export default async function handler(req, res) {
     const userId = String(message.from.id);
     const userName = message.from?.first_name || "User";
     const text = message.text.trim();
+    const lowerText = text.toLowerCase();
      const isReplyToBot =
   message.reply_to_message?.from?.is_bot === true;
 
