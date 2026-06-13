1dc0f3d56ab1ef416a06bafc51b130cbcc0295e6
Akash R <imakash566@gmail.com>
2026-06-13 13:40:21 +0530

    Add function to send messages via Telegram API

---
+++ api/lib/telegram.js
@@ -0,0 +1,14 @@
+export async function sendTelegram(chatId, text, replyTo) {
+  await fetch(
+    `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: chatId,
+        text,
+        reply_to_message_id: replyTo,
+      }),
+    }
+  );
+}
