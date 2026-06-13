de821421bd2f3e9c4d9b349f6637fc29fec02f3f
Akash R <imakash566@gmail.com>
2026-06-13 14:46:14 +0530

    Fix import path for supabase module

--- api/admin/messages.js
+++ api/admin/messages.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../lib/supabase.js";
+import { supabase } from "../lib/supabase.js";
 
 export default async function handler(req, res) {
   const password = req.headers["x-admin-password"];
