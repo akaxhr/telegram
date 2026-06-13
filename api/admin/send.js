3521d7e9d1ce4e903e905c0929aba983b1178f5a
Akash R <imakash566@gmail.com>
2026-06-13 14:46:31 +0530

    Update send.js

--- api/admin/send.js
+++ api/admin/send.js
@@ -1,5 +1,5 @@
-import { supabase } from "../../lib/supabase.js";
-import { sendTelegram } from "../../lib/telegram.js";
+import { supabase } from "../lib/supabase.js";
+import { sendTelegram } from "../lib/telegram.js";
 
 export default async function handler(req, res) {
   const password = req.headers["x-admin-password"];
